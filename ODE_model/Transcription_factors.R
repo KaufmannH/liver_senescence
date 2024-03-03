a12b4c3d7ee3412ef4d82f0af76d31c44d23edb2
KaufmannH <helene.kaufmann00@gmail.com>
2024-03-03 23:34:01 +0100

    add ODE folders

---
+++ ODE_model/Transcription_factors.R
@@ -0,0 +1,20 @@
+library('dorothea')
+library(dplyr)
+
+data = get(data('dorothea_mm', package="dorothea"))
+#print(unique(data$tf))
+
+# get target genes for tf
+trans_targets_nfkb1 = data[data$tf == 'NFKB1', ]
+trans_targets_nfkb2 = data[data$tf == 'NFKB2', ]
+print(trans_targets_nfkb2)
+
+# get tf for genes that rise
+tf_cytokine = data[data$target == 'Ccl2', ]
+
+Mode <- function(data) {
+  ux <- unique(data)
+  ux[which.max(tabulate(match(data, ux)))]
+}
+
+#df %>% group_by(a) %>% mutate(c=Mode(b))
